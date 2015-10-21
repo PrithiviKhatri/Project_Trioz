281971a4fb667aedf462a28fb9d1b939202e6a8f
prithivikhatri <prithivikhatri@gmail.com>
2015-10-21 03:42:50 -0500

    updated pom.xml with dependencies,updated web.xml for spring security , added spring login security

--- src/main/java/trioz/project/controller/LoginController.java
+++ src/main/java/trioz/project/controller/LoginController.java
@@ -1,17 +1,59 @@
 package trioz.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import trioz.project.domain.User;
+import trioz.project.service.UserService;
 
 @Controller
+@SessionAttributes("user")
 public class LoginController {
 
-	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
-	public String showAdminHome() {
-		System.out.println("inside show admin Home");
-		return "AdminHome";
+	@Autowired
+	UserService userservice;
+
+	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
+	public String login(Model model) {
+		System.out.println("inside login method!!!");
+		return "login";
+	}
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String welcome(Model model) {
+		System.out.println("inside welcome method");
+
+		User user = userservice.findUserByUserName(model.asMap().get("user").toString());
+		System.out.println("user is " + user);
+		model.addAttribute("user", user);
+		switch (user.getRole()) {
+		case "ROLE_ADMIN":
+			return "AdminHome";
+		case "ROLE_PROFESSOR":
+			return "ProfessorHome";
+		case "ROLE_STUDENT":
+			return "StudentHome";
+		}
+		return null; // need to change null to other page
+	}
+
+	@RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
+	public String loginerror(Model model) {
+
+		model.addAttribute("error", "true");
+		return "login";
 
 	}
-	
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(Model model, SessionStatus status) {
+		status.setComplete();
+		return "redirect:/login";
+	}
+
 }
